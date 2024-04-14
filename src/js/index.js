1e81e27be5ddb559c587d6055961c6ea0437eb49
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-04-14 15:42:05 +0200

    tweak on dialog

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import { showMenu } from "./nav/openNav.js";
 import { closeMenu } from "./nav/closeNav.js";
-import { modalEventListeners, addOpenModalListeners, addCloseModalListeners } from "./modals/index.js";
 
 showMenu();
 closeMenu();
-modalEventListeners();
-addOpenModalListeners();
-addCloseModalListeners();
