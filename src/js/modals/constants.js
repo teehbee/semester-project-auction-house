1f9c4dfa3d36f74d3e02bcc19d2ff8d6303ba9da
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-04-15 09:29:15 +0200

    add link from login modal to register

--- src/js/modals/constants.js
+++ src/js/modals/constants.js
@@ -3,3 +3,5 @@ export const loginModal = document.querySelector("#login-dialog");
 export const openLoginModal = document.querySelectorAll(".open-login-modal");
 
 export const closeLoginModal = document.querySelector("#close-login-dialog");
+
+export const closeLoginOpenRegisterModal = document.querySelector("#login-to-register-modal-switch");
