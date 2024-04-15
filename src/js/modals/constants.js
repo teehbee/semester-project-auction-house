28e1c7073af1df582939f914617d7f13a35e0326
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-04-15 10:21:34 +0200

    link between login and registration modal

--- src/js/modals/constants.js
+++ src/js/modals/constants.js
@@ -1,7 +1,9 @@
 export const loginModal = document.querySelector("#login-dialog");
+export const registerModal = document.querySelector("#register-dialog");
 
 export const openLoginModal = document.querySelectorAll(".open-login-modal");
 
 export const closeLoginModal = document.querySelector("#close-login-dialog");
+export const closeRegisterModal = document.querySelector("#close-registration-dialog");
 
 export const closeLoginOpenRegisterModal = document.querySelector("#login-to-register-modal-switch");
--- src/js/modals/modalHandling.js
+++ src/js/modals/modalHandling.js
@@ -1,6 +1,9 @@
 import { loginModal } from "./constants.js";
+import { registerModal } from "./constants.js";
 import { openLoginModal } from "./constants.js";
 import { closeLoginModal } from "./constants.js";
+import { closeLoginOpenRegisterModal } from "./constants.js";
+import { closeRegisterModal } from "./constants.js";
 
 // Open login modal
 
@@ -21,3 +24,18 @@ openLoginModal.forEach(function (openModalLink) {
 closeLoginModal.addEventListener("click", () => {
   loginModal.close();
 });
+
+// Close registration modal
+
+closeRegisterModal.addEventListener("click", () => {
+  registerModal.close();
+});
+
+// Switch form login to registration modal
+
+closeLoginOpenRegisterModal.addEventListener("click", () => {
+  loginModal.close();
+  registerModal.showModal();
+});
+
+// Return from registration modal to login modal
