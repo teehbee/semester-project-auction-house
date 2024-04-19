c7efa3fc9860f044ae42c7688da697ba15cdc31f
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-04-19 15:01:35 +0200

    add eventhandling for image gallery modal

---
+++ src/js/modals/imageGalleryModal.js
@@ -0,0 +1,45 @@
+import {
+  imageGalleryModal,
+  openImageGalleryModal,
+  closeImageGalleryModal,
+} from "./constants.js";
+
+// Open image gallery modal and add url
+
+const imageGalleryEventListeners = function (event, imageGalleryModal) {
+  event.preventDefault();
+  imageGalleryModal.showModal();
+
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set("modal", "image-gallery");
+  const newUrl = "?" + searchParams.toString();
+  history.pushState({}, "", newUrl);
+};
+
+openImageGalleryModal.forEach(function (openImageGalleryModalLink) {
+  openImageGalleryModalLink.addEventListener("click", function (event) {
+    imageGalleryEventListeners(event, imageGalleryModal);
+  });
+});
+
+// Close image gallery modal and remove url
+
+function removeImageGalleryModalQueryParam() {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.delete("modal");
+  history.replaceState({}, "", "?" + searchParams.toString());
+}
+
+closeImageGalleryModal.addEventListener("click", () => {
+  imageGalleryModal.close();
+  removeImageGalleryModalQueryParam();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    if (imageGalleryModal.open) {
+      imageGalleryModal.close();
+      removeImageGalleryModalQueryParam();
+    }
+  }
+});
