69e9084eec8d61590fcd9fd18f35d88848da8a26
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-05-02 11:33:18 +0200

    sorting works and 100 posts are displayed

--- src/js/user/listings.js
+++ src/js/user/listings.js
@@ -2,6 +2,7 @@ import { fetchListings } from "../api/fetchListings.js";
 import { searchPosts } from "../posts/listingsSearch.js";
 import { searchInput, searchSubmit } from "../posts/constants.js";
 import { calculateCountdown } from "../posts/countdown.js";
+import { listingTemplate } from "../posts/listingCardHtml.js";
 
 const listingsMainContainer = document.querySelector(
   "#listings-main-container",
@@ -54,27 +55,7 @@ function displayListings(input) {
       <p class="card-text">Time left: ${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes ${secondsLeft} seconds</p>
     `;
 
-    const listingHTML = `
-      <div class="listings-container col-12 col-sm-6 col-lg-4 col-xl-3">
-        <a class="text-decoration-none" href="/auctions/listing.html">
-          <div class="card card-width-306px border-none border-radius-none mx-auto">
-            <img class="listings-image-card p-3 object-fit-img" src="${listing.media[0]?.url || "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"}" alt="${listing.media[0]?.alt || "Image description is missing"}"/>
-            <div class="card-body">
-              <h2 class="card-title fs-1-25rem">${listing.title}</h2>
-              <p class="last-chance-frontpage-time-left card-text">Ends at: ${countdownHTML}</p>
-              <p class="card-text">
-                Next bid
-                <span class="card-next-bid-amount">${lastBid}</span>
-              </p>
-              <p class="card-text">
-                Seller:
-                <span class="card-listing-seller">${listing.seller.name}</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    `;
+    const listingHTML = listingTemplate(listing, countdownHTML, lastBid);
     listingsHTML += listingHTML;
   });
 
